d2320a0913405ca60ddb82e42f11681a66304e08
mnytc <85677164+Monytccc@users.noreply.github.com>
2024-07-15 22:24:39 +0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1152,18 +1152,3 @@ function detectBrowser () {
     return 'firefox'
   }
 }
-
-function checkClose() {
-    if (ok) {
-        return "Have you saved your information?";
-    }
-}
-
-var ok = true; // this is reset everytime a new page is loaded
-
-function okToLeave() { // called if leaving the page is needed
-    ok = false;
-}
-
-window.onbeforeunload = checkClose;
-window.onunload = checkClose;
